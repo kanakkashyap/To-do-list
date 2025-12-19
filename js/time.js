dad57ea4d185e77f1757baae2cedb96f0c380190
kanakkashyap <008kanak@gmail.com>
2025-12-19 15:42:48 +0530

    Update time.js

--- js/time.js
+++ js/time.js
@@ -1,11 +1,1 @@
-function updateTime() {
-    var dt = new Date();
-    // This updates the HTML element every time the function runs
-    document.getElementById("datetime").innerHTML = dt.toLocaleString();
-}
 
-// This tells the browser to run the function every 1000 milliseconds (1 second)
-setInterval(updateTime, 1000);
-
-// Run it once immediately so the screen isn't blank for the first second
-updateTime();
